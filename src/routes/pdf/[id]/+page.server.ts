9a26b5832cf2a1c2ea397e9b6145213f0635f4dc
jperedo <peredo01@gmail.com>
2024-05-29 12:05:32 -0500

    added document details

--- src/routes/pdf/[id]/+page.server.ts
+++ src/routes/pdf/[id]/+page.server.ts
@@ -1,21 +1,43 @@
-import { error } from '@sveltejs/kit';
+import { error, fail, redirect } from '@sveltejs/kit';
 import fs from 'fs/promises';
 import path from 'path';
+import { DocumentsProcessedSchema } from '$lib/documentsProcessed';
+import { documentsProcessed } from '$lib/documentsProcessed';
+import { message, superValidate } from 'sveltekit-superforms';
+import { zod } from 'sveltekit-superforms/adapters';
+
+
+
+
+const updateDocumentProcessedSchema = DocumentsProcessedSchema.extend({
+	id: DocumentsProcessedSchema.shape.id.optional(),
+	documentId: DocumentsProcessedSchema.shape.documentId.optional(),
+	pageCount: DocumentsProcessedSchema.shape.pageCount.optional(),
+	filename: DocumentsProcessedSchema.shape.filename.optional(),
+});
 
 export async function load({ params }) {
-	const filename = params.id;
-	const pdfPath = path.join('uploads', `${filename}.pdf`);
-	console.log(params);
+	const docDetails = documentsProcessed.find((d) => d.id == params.id);
 
+	if (params.id && !docDetails) throw error(404, 'Document details not found.');
+
+	const form = await superValidate(docDetails, zod(DocumentsProcessedSchema));
+	const filename = docDetails?.filename;
+	const base64EncodedPDF = filename ? await getPdf(filename) : undefined
+	return {
+		form,
+		// docDetails,
+		pdfData: base64EncodedPDF,
+	};
+
+}
+
+async function getPdf(filename: string) {
+	const pdfPath = path.join('uploads', `${filename}`);
 	try {
 		const pdfBuffer = await fs.readFile(pdfPath);
-		const base64EncodedPDF = pdfBuffer.toString('base64');
+		return pdfBuffer.toString('base64')
 
-		return {
-			body: {
-				pdfData: base64EncodedPDF,
-			},
-		};
 	} catch (err) {
 		if ((err as NodeJS.ErrnoException).code === 'ENOENT') {
 			throw error(404, 'PDF not found');
@@ -24,3 +46,40 @@ export async function load({ params }) {
 		}
 	}
 }
+
+export const actions = {
+	default: async ({ request }) => {
+		console.log('HERHEHREHE')
+		const formData = await request.formData();
+		const form = await superValidate(formData, zod(updateDocumentProcessedSchema));
+
+		if (!form.valid) return fail(400, { form });
+
+		if (!form.data.id) {
+			throw error(405, 'New records cannot be created in this form as they are created automatically');
+
+		} else {
+			// UPDATE user
+			const index = documentsProcessed.findIndex((d) => d.id == form.data.id);
+			if (index == -1) throw error(404, 'Document not found.');
+
+			if (formData.has('delete')) {
+				// DELETE user
+				const parentDocumentId = documentsProcessed[index].documentId
+				documentsProcessed.splice(index, 1);
+				throw redirect(303, '/document/'.concat(parentDocumentId));
+
+			} else {
+				documentsProcessed[index].docText = form.data.docText;
+				documentsProcessed[index].updatedAt = new Date();
+
+				const updatedForm = await superValidate(documentsProcessed[index], zod(DocumentsProcessedSchema));
+
+				if (!updatedForm.valid) return fail(400, { updatedForm });
+				return message(updatedForm, 'Document updated');
+			}
+		}
+
+		// return { form };
+	}
+};
--- src/routes/pdf/[id]/+page.svelte
+++ src/routes/pdf/[id]/+page.svelte
@@ -1,6 +1,20 @@
 <script lang="ts">
-	export let data;
+	import type { PageData } from './$types.js';
+	import { page } from '$app/stores';
+	import { superForm } from 'sveltekit-superforms';
+	import  SuperDebug  from 'sveltekit-superforms';
+	export let data: PageData;
+
+	const { form, errors, constraints, enhance, delayed, message } = superForm(
+		data.form, {
+			resetForm: false
+		}
+	);
 </script>
+
+{#if $message}
+	<h3 class:invalid={$page.status >= 400}>{$message}</h3>
+{/if}
 
 <div class="grid grid-cols-2 gap-4">
 	<iframe title="PDF Viewer" width=100% height="315"
@@ -21,11 +35,77 @@
 					</style>
 				</head>
 				<body>
-					<iframe src="data:application/pdf;base64,${data.body.pdfData}"></iframe>
+					<iframe src="data:application/pdf;base64,${data.pdfData}"></iframe>
 				</body>
 			</html>
 		`}>
 	</iframe>
 
-	<div>Hello</div>
+	<div>Hello
+
+		<div>
+			<form method="POST" use:enhance >
+
+				<input type="hidden" id="id" name="id" bind:value={$form.id} />
+
+				<span>
+					<label for="language" class="label label-text font-semibold" >Language: </label>
+					<input
+						name="language"
+						id="language"
+						aria-invalid={$errors.language ? 'true' : undefined}
+						bind:value={$form.language}
+						{...$constraints.language}
+						class="input input-bordered input-sm w-full max-w-xs"
+						readonly
+					/>
+					{#if $errors.language}<span class="invalid">{$errors.language}</span>{/if}
+				</span>
+				<span>
+					<label for="pageCount" class="label label-text font-semibold">No. of pages: </label>
+					<input
+						name="pageCount"
+						id="pageCount"
+						aria-invalid={$errors.pageCount ? 'true' : undefined}
+						bind:value={$form.pageCount}
+						{...$constraints.pageCount}
+						class="input input-bordered input-sm w-full max-w-xs"
+						readonly
+					/>
+					{#if $errors.pageCount}<span class="invalid">{$errors.pageCount}</span>{/if}
+				</span>
+				<span>
+					<label for="docText" class="label label-text font-semibold">Document Text: </label>
+					<textarea
+						name="docText"
+						id="docText"
+						aria-invalid={$errors.docText ? 'true' : undefined}
+						bind:value={$form.docText}
+						{...$constraints.docText}
+						class="textarea textarea-bordered w-full max-w-xs"/>
+					{#if $errors.docText}<span class="invalid">{$errors.docText}</span>{/if}
+				</span>
+
+				<div>
+					<button name="submit" id="submit" class="btn">Submit</button> {#if $delayed}Working...{/if}
+					{#if $form.id}
+						<button
+							name="delete" id="delete"
+							on:click={(e) => !confirm('Are you sure?') && e.preventDefault()}
+							class="btn btn-neutral">Delete</button>
+					{/if}
+				</div>
+
+			</form>
+		</div>
+
+	</div>
 </div>
+
+<SuperDebug data={$form} />
+
+<style>
+    .invalid {
+        color: red;
+    }
+</style>
