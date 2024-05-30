f8823b3f7247034e980edb5dc13f180561dfd2a9
jperedo <peredo01@gmail.com>
2024-05-30 10:18:34 -0500

    initial commit

---
+++ src/lib/documentAudio.ts
@@ -0,0 +1,44 @@
+import { z } from 'zod';
+
+
+export const DocumentAudioSchema = z.object({
+	id: z.string().uuid(),
+	documentsProcessedId: z.string().uuid(),
+	audioFilePath: z.string().url(),
+	documentPageNumber: z.number(),
+	voiceName: z.string().min(1).max(50),
+	createdAt: z.date().default(() => new Date()), // Date doc was first processed
+	updatedAt: z.date().default(() => new Date()).optional(),
+})
+
+type documentVoicesDB = z.infer<typeof DocumentAudioSchema>[]
+
+export const DocumentVoices: documentVoicesDB = [
+	{
+		id: '7a7f3dd5-9a61-41ce-853f-2b6345362341',
+		documentsProcessedId: '8a7f3dd5-9a61-41ce-853f-2b6345362341',
+		audioFilePath: '/data/file1.mpp3',
+		documentPageNumber: 1,
+		voiceName: 'Jenny',
+		createdAt: new Date(),
+		updatedAt: new Date(),
+	},
+	{
+		id: '7a7f3dd5-9a61-41ce-853f-2b6345362342',
+		documentsProcessedId: '8a7f3dd5-9a61-41ce-853f-2b6345362341',
+		audioFilePath: '/data/file2.mpp3',
+		documentPageNumber: 2,
+		voiceName: 'Jenny',
+		createdAt: new Date(),
+		updatedAt: new Date(),
+	},
+	{
+		id: "7a7f3dd5-9a61-41ce-853f-2b6345362343",
+		documentsProcessedId: "8a7f3dd5-9a61-41ce-853f-2b6345362342",
+		audioFilePath: '/data/file3.mpp3',
+		documentPageNumber: 1,
+		voiceName: 'Jenny',
+		createdAt: new Date(),
+		updatedAt: new Date(),
+	},
+]
