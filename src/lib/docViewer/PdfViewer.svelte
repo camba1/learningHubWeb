753a94b996c8ba26666adfbc1a0deaeb486b059d
jperedo <peredo01@gmail.com>
2024-06-05 14:33:56 -0500

    initial commit

---
+++ src/lib/docViewer/PdfViewer.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	export let pdfData;
+</script>
+
+<iframe title="PDF Viewer" width=100% height="100%"
+				srcdoc={`
+			<html lang="en">
+				<head>
+					<title>Embedded PDF</title>
+					<style>
+						body {
+							margin: 0;
+						}
+						iframe {
+							display: block;
+							width: 100%;
+							height: 100vh;
+							border: none;
+						}
+					</style>
+				</head>
+				<body>
+					<iframe src="data:application/pdf;base64,${pdfData}"></iframe>
+				</body>
+			</html>
+		`}>
+</iframe>
