134158f8e78b91432c50945924657bcb8829c0c5
jperedo <peredo01@gmail.com>
2024-06-05 14:35:57 -0500

    moved the bulk of the code to components

--- src/routes/pdf/[id]/+page.svelte
+++ src/routes/pdf/[id]/+page.svelte
@@ -4,7 +4,10 @@
 	import { superForm } from 'sveltekit-superforms';
 	// import  SuperDebug  from 'sveltekit-superforms';
 	import AudioPlayer from '$lib/audioPlayer/AudioPlayer.svelte';
-	import { CircleArrowLeft } from 'lucide-svelte';
+	import PdfViewer from '$lib/docViewer/PdfViewer.svelte';
+	import TextField from '$lib/form/TextField.svelte';
+	import TextAreaField from '$lib/form/TextAreaField.svelte';
+	import FormButtons from '$lib/form/FormButtons.svelte';
 
 	export let data: PageData;
 
@@ -13,6 +16,10 @@
 			resetForm: false
 		}
 	);
+
+	const searchPageUrl: string = ''.concat("/document/", $form.documentId);
+	const btnLabels = {"submitLbl": "Submit", "deleteLbl": "Delete", "backLbl": "Back", "confirmationDelMsg": "Delete document version "};
+
 </script>
 
 {#if $message}
@@ -20,29 +27,7 @@
 {/if}
 
 <div class="grid grid-cols-2 gap-4 p-4 content-center h-full">
-	<iframe title="PDF Viewer" width=100% height="100%"
-		srcdoc={`
-			<html lang="en">
-				<head>
-					<title>Embedded PDF</title>
-					<style>
-						body {
-							margin: 0;
-						}
-						iframe {
-							display: block;
-							width: 100%;
-							height: 100vh;
-							border: none;
-						}
-					</style>
-				</head>
-				<body>
-					<iframe src="data:application/pdf;base64,${data.pdfData}"></iframe>
-				</body>
-			</html>
-		`}>
-	</iframe>
+	<PdfViewer pdfData={data.pdfData}/>
 
 	<div>
 		<div id="docTabs" role="tablist" class="tabs tabs-lifted">
@@ -52,59 +37,22 @@
 					<form method="POST" use:enhance >
 
 						<input type="hidden" id="id" name="id" bind:value={$form.id} />
+						<TextField label="Language" id="language" bind:value={$form.language}
+											 errors={$errors.language} constraints={$constraints.language}
+												readOnly={true}/>
 
-						<span>
-					<label for="language" class="label label-text font-semibold" >Language: </label>
-					<input
-						name="language"
-						id="language"
-						aria-invalid={$errors.language ? 'true' : undefined}
-						bind:value={$form.language}
-						{...$constraints.language}
-						class="input input-bordered input-sm w-full max-w-xs"
-						readonly
-					/>
-							{#if $errors.language}<span class="invalid">{$errors.language}</span>{/if}
-				</span>
-						<span>
-					<label for="pageCount" class="label label-text font-semibold">No. of pages: </label>
-					<input
-						name="pageCount"
-						id="pageCount"
-						aria-invalid={$errors.pageCount ? 'true' : undefined}
-						bind:value={$form.pageCount}
-						{...$constraints.pageCount}
-						class="input input-bordered input-sm w-full max-w-xs"
-						readonly
-					/>
-							{#if $errors.pageCount}<span class="invalid">{$errors.pageCount}</span>{/if}
-				</span>
-						<span>
-					<label for="docText" class="label label-text font-semibold">Document Text: </label>
-					<textarea
-						name="docText"
-						id="docText"
-						aria-invalid={$errors.docText ? 'true' : undefined}
-						bind:value={$form.docText}
-						{...$constraints.docText}
-						class="textarea textarea-bordered w-full max-w-xs"/>
-							{#if $errors.docText}<span class="invalid">{$errors.docText}</span>{/if}
-				</span>
-
-						<div>
-							<button name="submit" id="submit" class="btn">Submit</button> {#if $delayed}Working...{/if}
-							{#if $form.id}
-								<button
-									name="delete" id="delete"
-									on:click={(e) => !confirm('Are you sure?') && e.preventDefault()}
-									class="btn btn-neutral">Delete</button>
-							{/if}
-							<a href="/document/{$form.documentId}" role="button" class="btn btn-sm"><CircleArrowLeft class="w-4 h-4"/>Back</a>
-						</div>
-
+						<TextField label="No. of pages" id="pageCount" bind:value={$form.pageCount}
+											 errors={$errors.pageCount} constraints={$constraints.pageCount}
+											 readOnly={true}/>
+						<TextAreaField label="Document Text:" id="docText" bind:value={$form.docText}
+													 errors={$errors.docText} constraints={$constraints.docText} />
+						<FormButtons submitLbl={btnLabels.submitLbl} deleteLbl={btnLabels.deleteLbl} backLbl={btnLabels.backLbl}
+												 delayed={$delayed} objectId={$form.id} confirmationDelMsg={''.concat(btnLabels.confirmationDelMsg, $form.language.toString(), "?")}
+												 backUrl={searchPageUrl}/>
 					</form>
 				</div>
 			</div>
+
 			<input type="radio" name="docDetailsTabsGroup" role="tab" class="tab" aria-label="Audio"  />
 			<div role="tabpanel" class="tab-content bg-base-100 border-base-300 rounded-box p-6">
 				<div class="p-4">
