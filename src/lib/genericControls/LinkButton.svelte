d9abb4e34e5ef58a7272f26062e648b6c0ed208f
jperedo <peredo01@gmail.com>
2024-06-06 13:54:06 -0500

    initial commit

---
+++ src/lib/genericControls/LinkButton.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	/**
+	 * @param icon The Lucide icon component
+	 * @param label The button label
+	 * @param href The link to navigate to
+	 */
+
+	export let icon; // This prop will accept any Lucide icon component
+	export let label: string;
+	export let href: string;
+</script>
+
+	<button class={`btn btn-xs mx-0.5`}>
+			<svelte:component this={icon} class="w-4 h-4" />
+			<a href={href}>{label}</a>
+	</button>
