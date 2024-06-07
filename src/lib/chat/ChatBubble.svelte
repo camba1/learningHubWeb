7c0c711d7050dd1e191c15d4e6a991c72a55d04e
jperedo <peredo01@gmail.com>
2024-06-07 15:57:50 -0500

    initial commit

---
+++ src/lib/chat/ChatBubble.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+import { Bot } from 'lucide-svelte';
+import { User } from 'lucide-svelte';
+
+export let  username: string;
+export let  message: string;
+export let  messageTime: string;
+export let  messageStatus: string;
+export let role: string;
+export let bubbleColor: string = "";
+
+</script>
+
+<div class="chat {role === 'user' ? 'chat-end' : 'chat-start'} ">
+	<div class="chat-image avatar">
+		<div class="w-10 rounded-full">
+			{#if (role === "user")}
+				<User class="w-10 h-10"/>
+			{:else}
+				<Bot class="w-10 h-10"/>
+			{/if}
+		</div>
+	</div>
+	<div class="chat-header">
+		{username}
+		<time class="text-xs opacity-50">{messageTime}</time>
+	</div>
+	<div class="chat-bubble {bubbleColor}">{message}</div>
+	<div class="chat-footer opacity-50">
+		{messageStatus}
+	</div>
+</div>
