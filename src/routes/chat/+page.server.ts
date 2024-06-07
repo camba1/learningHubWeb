0b129f44a4060dc5403c9616fefc01deabb36c82
jperedo <peredo01@gmail.com>
2024-06-07 16:35:02 -0500

    Cleanup

--- src/routes/chat/+page.server.ts
+++ src/routes/chat/+page.server.ts
@@ -1,46 +1,9 @@
-// import {messages, type MessageDB, MessageSchema} from '$lib/message';
-// import { message, superValidate } from 'sveltekit-superforms';
-// import { zod } from 'sveltekit-superforms/adapters';
-//
-//
-// export const load = async ( ) => {
-// 	const form: MessageDB = {
-// 		name: "user",
-// 		msg: "",
-// 		role: "user",
-// 		time: "12:45",
-// 		status: "delivered"
-// 	}
-//
-// 	return { form, messages };
-// }
-//
-// export const actions = {
-// 	default: async ({ request }) => {
-// 		const formData = await request.formData();
-// 		const form = await superValidate(formData, zod(MessageSchema));
-// 		console.log(form.data.msg)
-// 		return message(form, 'Document updated');
-// 	}
-// }
-
-// +page.server.ts
 import { superValidate } from 'sveltekit-superforms';
 import { fail } from '@sveltejs/kit';
-// import { z } from 'zod';
-// import type { Actions, PageServerLoad } from './$types';
 import { zod } from 'sveltekit-superforms/adapters';
 import {type MessageDB, MessageSchema} from '$lib/message';
 
 
-//
-// const schema = z.object({
-// 	messageText: z.string().trim().min(1),
-// 	role: z.enum(['user', 'assistant']),
-// });
-// export type myschema = z.infer<typeof schema>
-
-// const messages: myschema[] = [];
 const messages: MessageDB[] = [];
 
 export const load = async () => {
