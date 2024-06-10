21dec52f761d6d936d6a8e6bc5d3d5486c723a17
jperedo <peredo01@gmail.com>
2024-06-10 10:25:01 -0500

    cleanup, new scroll to bottom functionality

--- src/routes/chat/+page.svelte
+++ src/routes/chat/+page.svelte
@@ -4,10 +4,16 @@
 	import TextAreaField from '$lib/form/TextAreaField.svelte';
 	import type { PageData } from './$types.js';
 	import FormButtons from '$lib/form/FormButtons.svelte';
+	import { afterUpdate } from 'svelte';
 
 	export let data: PageData;
+
+	let element: HTMLDivElement;
 	const userBubbleColor = "chat-bubble-info";
 	const botBubbleColor = "";
+	const searchPageUrl: string = "/";
+	const btnLabels = {"submitLbl": "Submit", "deleteLbl": "Delete", "backLbl": "Back", "confirmationDelMsg": "Delete document "};
+
 
 
 	const { form, errors, constraints, enhance, delayed } = superForm(data.form, { resetForm: true,
@@ -22,16 +28,27 @@
 		}
 	});
 
-	const searchPageUrl: string = "/";
-	const btnLabels = {"submitLbl": "Submit", "deleteLbl": "Delete", "backLbl": "Back", "confirmationDelMsg": "Delete document "};
-
 	// Prevent Superforms from overwriting the form element value on form action return.
 	let tempMessage = $form.messageText;
 	$: $form.messageText = tempMessage;
 
+	
+	const scrollToBottom = async (node: HTMLDivElement) => {
+		node.scroll({ top: node.scrollHeight, behavior: 'smooth' });
+	};
+
+	afterUpdate(() => {
+		// Scroll to bottom when new messages are added.
+		if($form) scrollToBottom(element);
+	});
+
+	$: if ($form && element) {
+		scrollToBottom(element);
+	}
+
 </script>
 
-<div class="max-h-96 overflow-y-auto border border-grey-500/100 mx-5 p-2">
+<div bind:this={element} class="max-h-96 overflow-y-auto border border-grey-500/100 mx-5 p-2">
 	{#each data.messages as msg}
 
 		<ChatBubble username={msg.name} message={msg.messageText} role={msg.role}
@@ -40,7 +57,7 @@
 	{/each}
 </div>
 
-<form method="POST" use:enhance class=" mx-5">
+<form method="POST" use:enhance class="mx-5">
 
 	<TextAreaField  id="messageText" bind:value={tempMessage}
 											errors={$errors.messageText} constraints={$constraints.messageText}/>
@@ -48,12 +65,3 @@
 							 delayed={$delayed} objectId='chat' confirmationDelMsg={''.concat(btnLabels.confirmationDelMsg, 'chat', "?")}
 							 backUrl={searchPageUrl}/>
 </form>
-
-<!--<style>-->
-<!--    .messages-container {-->
-<!--        max-height: 500px; /* Define a fixed height */-->
-<!--        overflow-y: auto; /* Add vertical scroll if overflow */-->
-<!--        border: 1px solid #ccc; /* Optional border styling */-->
-<!--        padding: 10px; /* Optional padding */-->
-<!--    }-->
-<!--</style>-->
