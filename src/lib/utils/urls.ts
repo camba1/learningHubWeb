9a28622ef7189518bc0d835c8d1cc6bdc809a02a
jperedo <peredo01@gmail.com>
2024-06-11 13:20:08 -0500

    Initial Commit

---
+++ src/lib/utils/urls.ts
@@ -0,0 +1,11 @@
+export const InternalURLs = {
+	documents: "/documents",
+	document: "/document",
+	fileUpload: "/fileUpload",
+	about: "/about",
+	signIn: "/sign-in",
+	signUp: "/sign-up",
+	home: "/",
+	pdf: "/pdf",
+	audio: "/audio"
+}
