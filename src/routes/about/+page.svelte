81de86c013bd594108cc754539429ba7fd8d23de
jperedo <peredo01@gmail.com>
2024-06-11 10:31:44 -0500

    initial commit

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,40 @@
+<main>
+	<section id="about-us" class="container mx-auto pt-24 section-spacing">
+		<h2 class="text-3xl font-bold mb-8">About Us</h2>
+		<p>
+			At Bolbeck LLC, we believe that every child deserves a chance to explore the wonderful world of literature. That's why we created Bolbeck Learning, a platform powered by the latest AI technology that makes it easy for publishers and authors to distribute their books to kids around the world.
+		</p>
+		<p>
+			Our mission is to provide equal access to literature, no matter the language or location. With Bolbeck Learning, both parents and educators can introduce children to a diverse range of stories and characters, helping them develop a love for reading and learning.
+		</p>
+	</section>
+
+	<section id="imageHolder" class="container mx-auto section-spacing">
+		<img
+			src="/images/bolbeckLearning5.jpg"
+			class="rounded-lg shadow-lg my-auto mx-auto size-2/6"
+			alt="Kid listing to an audiobook"
+		/>
+	</section>
+
+	<section id="footer">
+		<footer class="container mx-auto pt-24 py-4">
+			<div class="flex flex-col sm:flex-row items-center sm:items-stretch justify-center sm:justify-between">
+				<div class="pr-8 mb-4 sm:mb-0">
+					<h2 class="text-xl font-bold">BolbeckLearning</h2>
+					<p class="text-sm text-gray-600">Learning for kids of all ages</p>
+				</div>
+				<div class="sm:flex sm:flex-row sm:items-stretch justify-center sm:justify-end">
+					<p class="text-sm text-gray-600">© 2024 Bolbeck LLC. All rights reserved.</p>
+				</div>
+			</div>
+		</footer>
+	</section>
+</main>
+
+
+<style>
+    .section-spacing {
+        margin-bottom: 2.75rem;
+    }
+</style>
