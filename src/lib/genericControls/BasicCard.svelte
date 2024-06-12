fea3cd12ee74f4badec1230c324d102cfea2147e
jperedo <peredo01@gmail.com>
2024-06-12 10:27:01 -0500

    initial commit

---
+++ src/lib/genericControls/BasicCard.svelte
@@ -0,0 +1,11 @@
+<script>
+	export let title;
+	export let description;
+</script>
+
+<div class="card bg-base-100 shadow-xl">
+	<div class="card-body">
+		<h3 class="card-title">{title}</h3>
+		<p>{description}</p>
+	</div>
+</div>
---
+++ src/lib/genericControls/MainFooter.svelte
@@ -0,0 +1,14 @@
+<footer class="container mx-auto pt-24 py-4">
+	<div class="flex flex-col sm:flex-row items-center sm:items-stretch justify-center sm:justify-between">
+		<div class="pr-8 mb-4 sm:mb-0">
+			<h2 class="text-xl font-bold">BolbeckLearning</h2>
+			<p class="text-sm text-gray-600">Learning for kids of all ages</p>
+		</div>
+		<div class="sm:flex sm:flex-row sm:items-stretch justify-center sm:justify-end">
+			<div>
+				<p class="text-sm text-gray-600">© 2024 Bolbeck LLC. All rights reserved</p>
+				<p class="text-sm text-gray-600">Contact us at opportunities@bolbeck.com</p>
+			</div>
+		</div>
+	</div>
+</footer>
