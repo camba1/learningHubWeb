a112f3e1c7d74ade88128be4fb907f465f809234
jperedo <peredo01@gmail.com>
2024-06-12 10:27:36 -0500

    Moved some code to components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+	import MainFooter from '$lib/genericControls/MainFooter.svelte';
+	import BasicCard from '$lib/genericControls/BasicCard.svelte';
+</script>
 <div>
 <main>
 	<section id="hero" class="container mx-auto pt-24">
@@ -25,34 +29,11 @@
 	<section id="features" class="container mx-auto pt-24">
 		<h2 class="text-3xl font-bold mb-8">Features</h2>
 
-		<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-			<div class="card bg-base-100 shadow-xl">
-				<div class="card-body">
-					<h3 class="card-title">PDF & MS Word</h3>
-					<p>Process PDF and MS Word versions of the books easily with our platform.</p>
-				</div>
-			</div>
-
-			<div class="card bg-base-100 shadow-xl">
-				<div class="card-body">
-					<h3 class="card-title">Translation</h3>
-					<p>Translate books to Spanish or French, making them accessible to more readers.</p>
-				</div>
-			</div>
-
-			<div class="card bg-base-100 shadow-xl">
-				<div class="card-body">
-					<h3 class="card-title">Audio</h3>
-					<p>Create audio versions of the books for listening on the go or in the car.</p>
-				</div>
-			</div>
-
-			<div class="card bg-base-100 shadow-xl">
-				<div class="card-body">
-					<h3 class="card-title">RAG-based Chat</h3>
-					<p>Enable chat with books via RAG, so kids can interact with their favorite characters.</p>
-				</div>
-			</div>
+		<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+			<BasicCard title="PDF & MS Word" description="Process PDF and MS Word versions of the books easily with our platform."/>
+			<BasicCard title="Translation" description="Translate books to Spanish or French, making them accessible to more readers."/>
+			<BasicCard title="Audio" description="Create audio versions of the books for listening on the go or in the car."/>
+			<BasicCard title="RAG-based Chat" description="Enable chat with books via RAG, so kids can interact with their favorite characters."/>
 		</div>
 
 		<div class="flex flex-wrap items-center mt-12">
@@ -72,18 +53,7 @@
 			</div>
 		</div>
 	</section>
-
-	<footer class="container mx-auto pt-24 py-4">
-		<div class="flex flex-col sm:flex-row items-center sm:items-stretch justify-center sm:justify-between">
-			<div class="pr-8 mb-4 sm:mb-0">
-				<h2 class="text-xl font-bold">BolbeckLearning</h2>
-				<p class="text-sm text-gray-600">Learning for kids of all ages</p>
-			</div>
-			<div class="sm:flex sm:flex-row sm:items-stretch justify-center sm:justify-end">
-				<p class="text-sm text-gray-600">© 2024 Bolbeck LLC. All rights reserved.</p>
-			</div>
-		</div>
-	</footer>
+	<MainFooter />
 </main>
 </div>
 
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,5 +1,9 @@
+<script lang="ts">
+	import MainFooter from '$lib/genericControls/MainFooter.svelte';
+</script>
+
 <main>
-	<section id="about-us" class="container mx-auto pt-24 section-spacing">
+	<section id="about-us" class="container mx-auto pt-12 section-spacing">
 		<h2 class="text-3xl font-bold mb-8">About Us</h2>
 		<p>
 			At Bolbeck LLC, we believe that every child deserves a chance to explore the wonderful world of literature. That's why we created Bolbeck Learning, a platform powered by the latest AI technology that makes it easy for publishers and authors to distribute their books to kids around the world.
@@ -16,20 +20,7 @@
 			alt="Kid listing to an audiobook"
 		/>
 	</section>
-
-	<section id="footer">
-		<footer class="container mx-auto pt-24 py-4">
-			<div class="flex flex-col sm:flex-row items-center sm:items-stretch justify-center sm:justify-between">
-				<div class="pr-8 mb-4 sm:mb-0">
-					<h2 class="text-xl font-bold">BolbeckLearning</h2>
-					<p class="text-sm text-gray-600">Learning for kids of all ages</p>
-				</div>
-				<div class="sm:flex sm:flex-row sm:items-stretch justify-center sm:justify-end">
-					<p class="text-sm text-gray-600">© 2024 Bolbeck LLC. All rights reserved.</p>
-				</div>
-			</div>
-		</footer>
-	</section>
+	<MainFooter />
 </main>
 
 
