5e4267faf0e5168e7661ca1095602cfe1af0598e
jperedo <peredo01@gmail.com>
2024-06-17 11:44:46 -0500

    initial commit

---
+++ src/lib/server/utils/externalUrls.ts
@@ -0,0 +1,8 @@
+import { env } from '$env/dynamic/private'
+
+const backendUrl:string= env.BACKEND_URL? env.BACKEND_URL:"http://127.0.0.1:8000"
+
+export const ExternalURLs = {
+	backendUrl: backendUrl,
+	agent: backendUrl.concat("/agent")
+}
