834710d3478270d912ae83d16f5b4c47f344453e
jperedo <peredo01@gmail.com>
2024-07-18 17:36:55 -0500

    enable CRUD with DB

--- src/routes/private/document/[[id]]/+page.server.ts
+++ src/routes/private/document/[[id]]/+page.server.ts
@@ -2,12 +2,12 @@ import { DocumentSchema } from '$lib/documents';
 import { superValidate, message } from 'sveltekit-superforms';
 import { zod } from 'sveltekit-superforms/adapters';
 import { error, fail, redirect } from '@sveltejs/kit';
-import { documents } from '$lib/documents';
-import { newId } from '$lib/idGenerator'
 import { type DocumentsProcessedLookupdDB } from '$lib/documentsProcessed';
 import { InternalURLs } from '$lib/utils/urls';
-import { fetchDocument, fetchDocumentDetailsLookup } from '$lib/api/documents';
+import { fetchDocument, fetchDocumentDetailsLookup, createDocument, updateDocument, deleteDocument } from '$lib/api/documents';
+import { z } from 'zod';
 
+type Document = z.infer<typeof DocumentSchema>;
 
 // Make the [id] optional when we are creating a new record
 const crudDocumentSchema = DocumentSchema.extend({
@@ -33,8 +33,6 @@ export const load = async ({ params }) => {
 
 	// If document is null, default values for the schema will be returned.
 	const form = await superValidate(doc, zod(crudDocumentSchema));
-	console.log(form)
-
 	return { form, docProcessedLookups };
 };
 
@@ -45,26 +43,30 @@ export const actions = {
 		const form = await superValidate(formData, zod(crudDocumentSchema));
 		if (!form.valid) return fail(400, { form });
 
+		console.log(form.data);
 		if (!form.data._key) {
 			// CREATE Document
-			const newDocId = newId()
-			const doc = { ...form.data, _key: newDocId };
-			documents.push(doc);
+
+			const doc: Document = { ...form.data } as Document;
+			const response = await createDocument(doc);
 
 			// return message(form, 'Document created');
-			throw redirect(303, InternalURLs.document.concat('/',newDocId));
+			throw redirect(303, InternalURLs.document.concat('/',response._key));
 
 		} else {
-			// UPDATE document
-			const index = documents.findIndex((d) => d._key == form.data._key);
-			if (index == -1) throw error(404, 'Document not found.');
+			// Modify document
 
 			if (formData.has('delete')) {
 				// DELETE document
-				documents.splice(index, 1);
+
+				await deleteDocument(form.data._key);
 				throw redirect(303, InternalURLs.documents);
 			} else {
-				documents[index] = { ...form.data, _key: form.data._key };
+
+				// UPDATE document
+				const doc2: Document = { ...form.data} as Document;
+				console.log(doc2);
+				await updateDocument(form.data._key, doc2);
 				return message(form, 'Document updated');
 			}
 		}
