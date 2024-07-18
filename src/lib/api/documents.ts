8b27f82e37d5c770a79130a28651e39f3424fc3e
jperedo <peredo01@gmail.com>
2024-07-18 10:26:06 -0500

    initial commit

---
+++ src/lib/api/documents.ts
@@ -0,0 +1,59 @@
+import { error } from '@sveltejs/kit';
+import { ExternalURLs } from '$lib/server/utils/externalUrls';
+import { z } from 'zod';
+import { DocumentSchema } from '$lib/documents';
+const DOCUMENTS_URL = ExternalURLs.documents;
+
+type Document = z.infer<typeof DocumentSchema>[];
+
+function get_header() {
+	const headers = new Headers();
+	headers.append('Content-Type', 'application/json');
+	headers.append('x-token','secret-token');
+	return headers;
+}
+
+export async function fetchDocument(id: string) {
+	const response = await fetch(`${DOCUMENTS_URL}/${id}`,
+		{ headers: get_header() }
+		);
+	if (!response.ok) {
+		throw error(response.status, await response.text());
+	}
+	return response.json();
+}
+
+export async function createDocument(data: Document) {
+	const response = await fetch(`${DOCUMENTS_URL}`, {
+		method: 'POST',
+		headers: get_header(),
+		body: JSON.stringify(data)
+	});
+	if (!response.ok) {
+		throw error(response.status, await response.text());
+	}
+	return response.json();
+}
+
+export async function updateDocument(id: string, data: Document) {
+	const response = await fetch(`${DOCUMENTS_URL}/${id}`, {
+		method: 'PUT',
+		headers: get_header(),
+		body: JSON.stringify(data)
+	});
+	if (!response.ok) {
+		throw error(response.status, await response.text());
+	}
+	return response.json();
+}
+
+export async function deleteDocument(id: string) {
+	const response = await fetch(`${DOCUMENTS_URL}/${id}`, {
+		method: 'DELETE',
+		headers: get_header()
+	});
+	if (!response.ok) {
+		throw error(response.status, await response.text());
+	}
+	return response.status;
+}
