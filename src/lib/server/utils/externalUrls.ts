389df313430e65c62db75dd3e81bbe424ed07a57
jperedo <peredo01@gmail.com>
2024-07-18 14:56:40 -0500

    Added doc details lookup url

--- src/lib/server/utils/externalUrls.ts
+++ src/lib/server/utils/externalUrls.ts
@@ -5,6 +5,7 @@ const backendUrl:string= env.BACKEND_URL
 export const ExternalURLs = {
 	backendUrl: backendUrl,
 	documents: backendUrl.concat("/documents"),
+	document_details_lookup: backendUrl.concat("/documents/{document_key}/document_details_lookup"),
 	document_details: backendUrl.concat("/document_details"),
 	users: backendUrl.concat("/users"),
 	organizations: backendUrl.concat("/organizations")
