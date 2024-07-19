c9f30eadb9921e49370605d72c4403474ce43074
jperedo <peredo01@gmail.com>
2024-07-19 12:15:18 -0500

    initial commit

---
+++ src/lib/api/apiClient.ts
@@ -0,0 +1,98 @@
+import type { ZodSchema } from 'zod';
+import { error } from '@sveltejs/kit';
+import { DEFAULT_SKIP, DEFAULT_LIMIT } from '$lib/server/utils/externalUrls';
+
+// const DEFAULT_SKIP = 0;
+// const DEFAULT_LIMIT = 25;
+
+export class APIClient<T> {
+	baseURL: string;
+	schema: ZodSchema<T>; // Accept a schema for validation
+
+	constructor(baseURL: string, schema: ZodSchema<T>) {
+		this.baseURL = baseURL;
+		this.schema = schema;
+	}
+
+	private getHeader() {
+		const headers = new Headers();
+		headers.append('Content-Type', 'application/json');
+		headers.append('x-token', 'secret-token');
+		return headers;
+	}
+
+	private async fetchResource(url: string, method: string = 'GET', body:  T | null = null) {
+		const options: RequestInit = {
+			method,
+			headers: this.getHeader(),
+		};
+
+		if (body) {
+			options.body = JSON.stringify(body);
+		}
+
+		const response = await fetch(url, options);
+
+		if (!response.ok) {
+			throw error(response.status, await response.text());
+		}
+
+		return  await response.json();
+	}
+
+	fetchItem(id: string): Promise<T> {
+		return this.fetchResource(`${this.baseURL}/${id}`);
+	}
+
+	createItem(data: T): Promise<T> {
+		return this.fetchResource(`${this.baseURL}`, 'POST', data);
+	}
+
+	updateItem(id: string, data: T): Promise<T> {
+		return this.fetchResource(`${this.baseURL}/${id}`, 'PUT', data);
+	}
+
+	async deleteItem(id: string): Promise<number> {
+		const options: RequestInit = {
+			method: 'DELETE',
+			headers: this.getHeader()
+		};
+
+		const response = await fetch(`${this.baseURL}/${id}`, options);
+
+		if (!response.ok) {
+			throw error(response.status, await response.text());
+		}
+
+		// No need to return a body for delete operation
+		return response.status;
+	}
+
+	fetchItems(skip = DEFAULT_SKIP, limit = DEFAULT_LIMIT, options: { [key: string]: string } = {}) {
+
+		const params = this.build_query_string(skip, limit, options);
+
+		return this.fetchResource(`${this.baseURL}?${params.toString()}`);
+	}
+
+	fetchDetailLookup(id: string, detailsURL: string, skip = DEFAULT_SKIP, limit = DEFAULT_LIMIT, options: { [key: string]: string } = {}) {
+		const url = detailsURL.replace('{key}', id);
+		const params = this.build_query_string(skip, limit, options);
+
+		return this.fetchResource(`${url}?${params.toString()}`);
+	}
+
+	private build_query_string(skip: number, limit: number, options: { [p: string]: string }) {
+
+		const params = new URLSearchParams();
+
+		params.append('skip', skip.toString());
+		params.append('limit', limit.toString());
+		Object.keys(options).forEach((key) => {
+			if (options[key]) {
+				params.append(key, options[key]);
+			}
+		});
+		return params;
+	}
+}
