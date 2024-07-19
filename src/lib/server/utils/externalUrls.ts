3c8c82b828ea324e26275a7c4adf3dc02aca8d48
jperedo <peredo01@gmail.com>
2024-07-19 12:15:03 -0500

    Added default skip and limit

--- src/lib/server/utils/externalUrls.ts
+++ src/lib/server/utils/externalUrls.ts
@@ -1,11 +1,14 @@
 import { env } from '$env/dynamic/private'
 
+export const DEFAULT_SKIP = 0;
+export const DEFAULT_LIMIT = 25;
+
 const backendUrl:string= env.BACKEND_URL
 
 export const ExternalURLs = {
 	backendUrl: backendUrl,
 	documents: backendUrl.concat("/documents"),
-	document_details_lookup: backendUrl.concat("/documents/{document_key}/document_details_lookup"),
+	document_details_lookup: backendUrl.concat("/documents/{key}/document_details_lookup"),
 	document_details: backendUrl.concat("/document_details"),
 	users: backendUrl.concat("/users"),
 	organizations: backendUrl.concat("/organizations")
