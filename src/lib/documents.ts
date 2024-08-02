2e14e385c5ba99e989ef86992a713c69aa070d94
jperedo <peredo01@gmail.com>
2024-08-02 12:56:44 -0500

    Increased th number of characters for a doc summary and updated age groups list

--- src/lib/documents.ts
+++ src/lib/documents.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 
 const DocumentTypeEnum = z.enum(['book', 'article', 'other'])
-const DocumentAgeGroupEnum = z.enum(['toddler', 'youth', 'young adult'])
+const DocumentAgeGroupEnum = z.enum(['Toddler', 'Early Reader', 'Young Reader', 'Young Adult'])
 
 // Document Schema
 export const DocumentSchema = z.object({
@@ -10,10 +10,14 @@ export const DocumentSchema = z.object({
 	title: z.string()
 		.min(1, "Title cannot be empty")
 		.max(250, "Title cannot exceed 250 characters"), // Title of the document
-	summary: z.string().max(500, "The max size of the summary is 500 characters"), // summary of the document
+	summary: z.string().max(2500, "The max size of the summary is 2500 characters"), // summary of the document
 	type: DocumentTypeEnum,
 	ageGroup: DocumentAgeGroupEnum,
 	authorId: z.string().optional(), // Id for the unique author identification
+	authorName: z.string().default(""),
+	filePath: z.string().min(5, 'File path must be at least 5 characters, including file extension'), // path relative to output folder
+	filename: z.string().min(5, 'Filename must be at least 5 characters, including file extension'),
+	characters: z.array(z.string()).optional(), //	Optional list of main document characters
 	tags: z.array(z.string()).optional(), // Optional list of tags/keywords
 	createdAt: z.date().default(() => new Date()).optional(), // Creation date of the document
 	updatedAt: z.date().default(() => new Date()).optional(), //  Updated date for the document
@@ -30,7 +34,7 @@ export const documents: DocumentSchemaType[] = [
 		title: "My First Document",
 		summary: "This is the summary of the document.",
 		type: "article",
-		ageGroup: "toddler",
+		ageGroup: "Toddler",
 		authorId: "8a7f3dd5-9a61-41ce-853f-2b6345362346",
 		createdAt: new Date(),
 		updatedAt: new Date(),
