2ddfa1e2f51b878d78cc54f13a25d25cc55f0663
jperedo <peredo01@gmail.com>
2024-08-04 11:49:10 -0500

    load pdf using file proxy rather than pdf data directly

--- src/lib/docViewer/PdfViewer.svelte
+++ src/lib/docViewer/PdfViewer.svelte
@@ -1,27 +1,9 @@
 <script lang="ts">
-	export let pdfData;
+	export let encodedFilename: string;
 </script>
 
-<iframe title="PDF Viewer" width=100% height="100%"
-				srcdoc={`
-			<html lang="en">
-				<head>
-					<title>Embedded PDF</title>
-					<style>
-						body {
-							margin: 0;
-						}
-						iframe {
-							display: block;
-							width: 100%;
-							height: 100vh;
-							border: none;
-						}
-					</style>
-				</head>
-				<body>
-					<iframe src="data:application/pdf;base64,${pdfData}"></iframe>
-				</body>
-			</html>
-		`}>
+<iframe
+	src={`/private/fileProxy?filename=${encodedFilename}`}
+	width=100% height="100%"
+	title="PDF Viewer">
 </iframe>
