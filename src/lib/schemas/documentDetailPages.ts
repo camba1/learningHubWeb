b4603092e7446cf1e36b3be0d6eaaf1057c919c3
jperedo <peredo01@gmail.com>
2024-08-09 09:41:07 -0500

    initial commit

---
+++ src/lib/schemas/documentDetailPages.ts
@@ -0,0 +1,22 @@
+import { z } from 'zod';
+
+
+const PageSchema = z.object({
+	friendlyName: z.string().readonly(),
+	number: z.number().readonly(),
+	filename: z.string().readonly(),
+	audioFilename: z.string().optional().readonly(),
+	speaker: z.string().optional().readonly()
+});
+
+export const DocumentDetailPagesSchema = z.object({
+	_key: z.string().readonly(), // Unique key for the document pages
+	docDetails_key: z.string().readonly(), // Unique  identification for parent document detail
+	pages: z.array(PageSchema).optional().readonly(),
+	createdAt: z.date().default(() => new Date()).optional().readonly(), // Date doc was first processed
+	updatedAt: z.date().default(() => new Date()).optional().readonly(), //  Latest date the doc was processed
+	usrMain_key_Create: z.string().optional().readonly(),
+	usrMain_key_Update: z.string().optional().readonly(),
+});
+
+export type DocumentDetailPagesSchemaType = z.infer<typeof DocumentDetailPagesSchema>;
