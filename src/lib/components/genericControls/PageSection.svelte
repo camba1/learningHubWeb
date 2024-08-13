9f0b91f4e92a5ad1ebc23fcc382542c42c298a29
jperedo <peredo01@gmail.com>
2024-08-13 12:15:47 -0500

    initial commit

---
+++ src/lib/components/genericControls/PageSection.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	export let label: string;
+</script>
+
+<div class="mt-6">
+	<div class="border-b mb-2">
+		<h2 class="font-semibold ">{label}</h2>
+	</div>
+	<div class="ml-10">
+		<slot />
+	</div>
+</div>
