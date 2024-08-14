4c7035564336b41ebb0602b0c85b996dada877f6
jperedo <peredo01@gmail.com>
2024-08-14 11:50:13 -0500

    Initial commit

---
+++ src/lib/components/genericControls/DispatchButton.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	/**
+	 * @param icon The Lucide icon component
+	 * @param label The button label
+	 * @param href The link to navigate to when the button is clicked
+	 * @param btn_class The button class. Default: "btn btn-xs mx-0.5"
+	 */
+
+	import type { ComponentType } from 'svelte';
+	import { createEventDispatcher } from 'svelte';
+
+	export let icon: ComponentType | null = null;
+	export let label: string;
+	export let btn_additional_class: string = "";
+
+	const dispatch = createEventDispatcher();
+
+</script>
+
+	<button class="btn btn-xs mx-0.5 {btn_additional_class}" on:click={() => dispatch('buttonClick')}>
+		{#if icon}
+			<svelte:component this={icon} class="w-4 h-4" />
+		{/if}
+		{label}
+	</button>
