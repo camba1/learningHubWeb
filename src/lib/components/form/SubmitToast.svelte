8a4432e60f34fbe04f9856500fa18a6aafbe564f
jperedo <peredo01@gmail.com>
2024-08-14 18:06:15 -0500

    Initial commit

---
+++ src/lib/components/form/SubmitToast.svelte
@@ -0,0 +1,28 @@
+
+<script lang="ts">
+	import { CircleX, SquareCheck } from 'lucide-svelte';
+
+	export let message:string | null;
+	export let page_status:number;
+
+	$: if (message) {
+		setTimeout(() => {
+			message = null;
+		}, 3000);
+	}
+
+</script>
+
+{#if message}
+	<div class="toast">
+		{#if (page_status >= 400)}
+			<div class="alert alert-error">
+				<CircleX class="w-5 h-5"/> {message}
+			</div>
+		{:else}
+			<div class="alert alert-success">
+				<SquareCheck class="w-5 h-5"/> {message}
+			</div>
+		{/if}
+	</div>
+{/if}
