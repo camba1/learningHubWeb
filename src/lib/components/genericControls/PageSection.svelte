a841e080c3c8a74407b1d1a127dc4da4fa2d6bb1
jperedo <peredo01@gmail.com>
2024-08-14 10:08:22 -0500

    Change div to section html tag

--- src/lib/components/genericControls/PageSection.svelte
+++ src/lib/components/genericControls/PageSection.svelte
@@ -2,11 +2,11 @@
 	export let label: string;
 </script>
 
-<div class="mt-6">
+<section  class="mt-6">
 	<div class="border-b mb-2">
 		<h2 class="font-semibold ">{label}</h2>
 	</div>
 	<div class="ml-10">
 		<slot />
 	</div>
-</div>
+</section>
