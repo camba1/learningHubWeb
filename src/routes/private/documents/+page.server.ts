9593dfd6daac01a54622648e7d54930803784cb5
jperedo <peredo01@gmail.com>
2024-08-18 11:45:16 -0500

    Add search and sorting functionality

--- src/routes/private/documents/+page.server.ts
+++ src/routes/private/documents/+page.server.ts
@@ -1,11 +1,79 @@
-import { DocumentSchema } from '$lib/schemas/documents';
+import type { DocumentSchemaType } from '$lib/schemas/documents';
+import {DocumentSchema} from '$lib/schemas/documents';
 import { fetchDocuments } from '$lib/api/documents';
 import { z } from 'zod';
+import { createGenericSearchParams } from '$lib/schemas/genericSearchParams';
+import { type Actions, fail } from '@sveltejs/kit';
+import { superValidate } from 'sveltekit-superforms/server';
+import { zod } from 'sveltekit-superforms/adapters';
 
-type Documents = z.infer<typeof DocumentSchema>[];
 
-export const load = async ({  params }) => {
-	const docs: Documents = await fetchDocuments();
-	return { documents: docs };
-	// return { documents };
+const sortByEnum = z.enum(['title', 'type', 'ageGroup']);
+const genericSearchParams = createGenericSearchParams(sortByEnum);
+
+const searchSchema = DocumentSchema.pick({
+	title: true,
+	type: true,
+	ageGroup: true,
+}).merge(genericSearchParams).partial();
+
+function getSearchParams(searchParams: URLSearchParams, paramName: string) {
+	return searchParams.get(paramName) || undefined;
+}
+
+export const load = async (params) => {
+	console.log("RELOAD")
+
+
+	const searchParams = params.url.searchParams;
+
+	const obj = {
+		skip: getSearchParams(searchParams,"skip"),
+		limit: getSearchParams(searchParams,"limit") ,
+		sort_by: getSearchParams(searchParams,"sort_by") ,
+		sort_order: getSearchParams(searchParams,"sort_order") ,
+		title: getSearchParams(searchParams,"title") ,
+		type: getSearchParams(searchParams,"type") ,
+		ageGroup: getSearchParams(searchParams,"ageGroup") ,
+
+	}
+	const form = await superValidate(searchSchema.parse(obj), zod(searchSchema));
+
+	console.log(form.data, form.valid)
+	const docs: DocumentSchemaType[] = await fetchDocuments(form.data.skip, form.data.limit,
+		form.data.sort_by , form.data.sort_order,
+		form.data.title, form.data.ageGroup,
+		form.data.type);
+
+	return {
+		form: form,
+		documents: docs,
+		sortByEnum: sortByEnum.options};
+};
+
+
+export const actions: Actions = {
+	default: async ({ request }) => {
+
+		const formData = await request.formData();
+		const form = await superValidate(formData, zod(searchSchema));
+		if (!form.valid) {
+			return fail(400, { form });
+		}
+
+		const { skip,
+			limit,
+			sort_by,
+			title, type,
+			ageGroup: age_group,
+			sort_order } = form.data
+
+
+		const docs: DocumentSchemaType[] = await fetchDocuments(skip, limit, sort_by, sort_order, title, age_group, type);
+		// console.log("docs:" ,docs)
+		// console.log("form: ",form)
+		return {
+			form,
+			documents: docs};
+	},
 };
--- src/routes/private/documents/+page.svelte
+++ src/routes/private/documents/+page.svelte
@@ -1,36 +1,87 @@
 <script lang="ts">
+
+	import type { PageData } from './$types.js';
+	import { page } from '$app/stores';
+	import { superForm } from 'sveltekit-superforms/client';
+
+
 	import { SquarePlus } from 'lucide-svelte';
 	import { FilePlus } from 'lucide-svelte';
 	import LinkButton from '$lib/components/genericControls/LinkButton.svelte';
 	import { InternalURLs } from '$lib/utils/urls';
+	import { DocumentTypeEnum, DocumentAgeGroupEnum } from '$lib/schemas/documents';
+	import FormSearchButtons from '$lib/components/form/FormSearchButtons.svelte';
+	import TextField from '$lib/components/form/TextField.svelte';
+	import SelectField from '$lib/components/form/SelectField.svelte';
+	import SubmitToast from '$lib/components/form/SubmitToast.svelte';
+	import FormPaginationButtons from '$lib/components/form/FormPaginationButtons.svelte';
+	import { sortOrderEnum } from '$lib/schemas/genericSearchParams';
 
-	export let data;
+	export let data: PageData;
+
+	const { form, errors, constraints, delayed, message } = superForm(data.form, {
+		resetForm: false
+	});
 
 </script>
 
-<div class="prose prose-headings p-3 px-3">
-	<h2>Documents</h2>
-</div>
-<div class="flex justify-end px-3">
-	<LinkButton icon={SquarePlus} label="New" href={InternalURLs.document} />
-	<LinkButton icon={FilePlus} label="New from file" href={InternalURLs.fileUpload} />
-</div>
-<div class="overflow-x-auto p-3">
-	<table class="table table-xs table-pin-rows table-pin-cols">
-		<thead>
-		<tr>
-			<th>Title</th>
-			<td>Type</td>
-			<td>Age Group</td>
-		</tr>
-		</thead>
-		<tbody>
-		{#each data.documents as doc}
-		<tr>
-			<th><a href="{InternalURLs.document}/{doc._key}">{doc.title}</a></th>
-			<td>{doc.type}</td>
-			<td>{doc.ageGroup}</td>
-		</tr>
-		{/each}
-	</table>
+<SubmitToast message={$message} page_status={$page.status}/>
+
+
+<div class="flex h-screen overflow-hidden">
+	<!-- Sidebar -->
+	<div id="search-drawer" class="w-64 flex-shrink-0 bg-base-200 p-4 border-r-2 overflow-y-auto">
+		<h2 class="text-xl font-bold mb-4">Search</h2>
+		<form method="GET" >
+			<input type="hidden" id="skip" name="skip" bind:value={$form.skip} />
+			<input type="hidden" id="limit" name="limit" bind:value={$form.limit} />
+			<TextField label="Title" id="title" bind:value={$form.title}
+								 errors={$errors.title} constraints={$constraints.title} />
+			<SelectField label="Type" id="type" bind:value={$form.type}
+									 errors={$errors.type} constraints={$constraints.type}
+									 optionValues={DocumentTypeEnum.options} />
+			<SelectField label="Age Group" id="ageGroup" bind:value={$form.ageGroup}
+									 errors={$errors.ageGroup} constraints={$constraints.ageGroup}
+									 optionValues={DocumentAgeGroupEnum.options} />
+			<SelectField label="Sort By" id="sort_by" bind:value={$form.sort_by}
+									 errors={$errors.sort_by} constraints={$constraints.sort_by}
+									 optionValues={data.sortByEnum} />
+			<SelectField label="Sort order" id="sort_order" bind:value={$form.sort_order}
+									 errors={$errors.sort_order} constraints={$constraints.sort_order}
+									 optionValues={sortOrderEnum.options} />
+			<FormSearchButtons submitLbl="Search" clearLbl="Clear" delayed={$delayed} reloadHref={InternalURLs.documents} />
+
+		</form>
+	</div>
+	<!-- Main content -->
+	<div class="flex-1 overflow-y-auto p-4">
+		<div class="flex justify-between items-center mb-4">
+			<h2 class="text-xl font-bold">Documents</h2>
+			<div>
+				<LinkButton icon={SquarePlus} label="New" href={InternalURLs.document} />
+				<LinkButton icon={FilePlus} label="New from file" href={InternalURLs.fileUpload} />
+			</div>
+		</div>
+		<div class="overflow-x-auto">
+			<table class="table table-sm table-pin-rows table-pin-cols">
+				<thead>
+				<tr>
+					<th>Title</th>
+					<td>Type</td>
+					<td>Age Group</td>
+				</tr>
+				</thead>
+				<tbody>
+				{#each data.documents as doc}
+					<tr>
+						<th><a href="{InternalURLs.document}/{doc._key}">{doc.title}</a></th>
+						<td>{doc.type}</td>
+						<td>{doc.ageGroup}</td>
+					</tr>
+				{/each}
+			</table>
+		</div>
+		<!-- Pagination -->
+		<FormPaginationButtons previousLbl="Previous" nextLbl="Next" delayed={$delayed} />
+	</div>
 </div>
