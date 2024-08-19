229fdba27611d26d66e196b7b424e2931fddd9f3
jperedo <peredo01@gmail.com>
2024-08-19 16:44:11 -0500

    Removed hardcoding of limits and skip

--- src/lib/schemas/genericSearchParams.ts
+++ src/lib/schemas/genericSearchParams.ts
@@ -1,4 +1,5 @@
 import { z, ZodEnum } from 'zod';
+import { DEFAULT_SKIP, DEFAULT_LIMIT } from '$lib/utils/urls';
 
 // Component is meant to be used in search forms. Provides the different usual options that all DB lookups
 // need. You need to pass an enum with the list of DB fields you want to be able to sort by as that varies.
@@ -9,8 +10,8 @@ export const createGenericSearchParams = <T extends [string, ...string[]]>(sortB
 	return z.object({
 		sort_by: sortByEnum.optional(),
 		sort_order: sortOrderEnum.optional(),
-		skip: z.number().int().min(0).optional(),
-		limit: z.number().int().min(25).optional()
+		skip: z.coerce.number().int().min(DEFAULT_SKIP).optional(),
+		limit: z.coerce.number().int().min(DEFAULT_LIMIT).optional()
 	});
 };
 
