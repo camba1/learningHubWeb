7ef8152c692faddf806f4556885918480655922f
jperedo <peredo01@gmail.com>
2024-08-20 10:17:44 -0500

    Added page number and dispatching the click event to parent component for each button

--- src/lib/components/form/FormPaginationButtons.svelte
+++ src/lib/components/form/FormPaginationButtons.svelte
@@ -6,10 +6,14 @@
 	 */
 
 	import { SquareChevronRightIcon, SquareChevronLeftIcon } from 'lucide-svelte';
+	import { createEventDispatcher } from 'svelte';
 
 	export let delayed: boolean;
 	export let previousLbl: string;
 	export let nextLbl: string;
+	export let pageNo: number;
+
+	const dispatch = createEventDispatcher();
 
 </script>
 
@@ -22,8 +26,13 @@ It includes icons and labels for user interaction and provides visual feedback f
 
 <!--<div class="join flex justify-end px-3 mt-4">-->
 	<div class="join flex justify-end px-3 mt-4">
-		<button name="submit" id="submit" class="join-item btn  btn-xs"> <SquareChevronLeftIcon class="w-4 h-4"/> {previousLbl}</button>
-		<button name="submit2" id="submit2" class="join-item btn  btn-xs">{nextLbl}  <SquareChevronRightIcon class="w-4 h-4"/></button>
+		<button name="submit" id="submit" class="join-item btn  btn-xs" on:click={() => dispatch('previousPage')}>
+			<SquareChevronLeftIcon class="w-4 h-4"/> {previousLbl}
+		</button>
+		<button class="join-item btn bg-base-300 btn-xs">Page {pageNo}</button>
+		<button name="submit2" id="submit2" class="join-item btn  btn-xs" on:click={() => dispatch('nextPage')}>
+			{nextLbl}  <SquareChevronRightIcon class="w-4 h-4"/>
+		</button>
 	</div>
 	{#if delayed}
 		<span class="loading loading-spinner text-neutral"></span>
