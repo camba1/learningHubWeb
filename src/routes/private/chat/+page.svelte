953baf5db60f1b0336318e7a8a7b458cca29e534
jperedo <peredo01@gmail.com>
2024-08-22 20:02:09 -0500

    Cleanup

--- src/routes/private/chat/+page.svelte
+++ src/routes/private/chat/+page.svelte
@@ -5,7 +5,7 @@
 	// export let data: PageData;
 
 	const searchPageUrl: string = "/";
-	const btnLabels = {"submitLbl": "Submit", "deleteLbl": "Clear", "backLbl": "Back", "confirmationDelMsg": "Clear our chat history? "};
+	const btnLabels = {"submitLbl": "Submit", "clearLbl": "Clear", "backLbl": "Back"};
 </script>
 
 <ChatUI {searchPageUrl} {btnLabels} />
