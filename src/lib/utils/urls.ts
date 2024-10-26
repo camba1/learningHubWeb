0e6fc4fbe50a8f999563d53ac7a18b0da634f8d6
jperedo <peredo01@gmail.com>
2024-10-26 11:40:57 -0500

    Added url for file proxy

--- src/lib/utils/urls.ts
+++ src/lib/utils/urls.ts
@@ -2,6 +2,7 @@ export const InternalURLs = {
 	documents: "/private/documents",
 	document: "/private/document",
 	fileUpload: "/private/fileUpload",
+	fileProxy: "/private/fileProxy",
 	characters: "/private/characters",
 	character: "/private/character",
 	about: "/about",
