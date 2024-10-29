1b8482d51bd5c25c15d1aa24095744e897c1f4d1
jperedo <peredo01@gmail.com>
2024-10-29 12:21:42 -0500

    Added links to doc and character pages

--- src/routes/private/document/[[id]]/document_character/[filename]/[doc_character_id]/+page.svelte
+++ src/routes/private/document/[[id]]/document_character/[filename]/[doc_character_id]/+page.svelte
@@ -8,20 +8,18 @@
 	import ImageViewer from '$lib/components/docViewer/ImageViewer.svelte';
 	import TextAreaField from '$lib/components/form/TextAreaField.svelte';
 	import SubmitToast from '$lib/components/form/SubmitToast.svelte';
-	import TextField from '$lib/components/form/TextField.svelte';
 	import { ImagePlus } from 'lucide-svelte';
 	import DispatchButton from '$lib/components/genericControls/DispatchButton.svelte';
 	import FieldLabel from '$lib/components/form/FieldLabel.svelte';
+	import LinkField from '$lib/components/genericControls/LinkField.svelte';
 
 	// import SuperDebug from 'sveltekit-superforms';
 
 
 	export let data: PageData;
-	const searchPageUrl: string = `${InternalURLs.document}/${data.document_id}`
-	console.log("data.document_id", data.document_id)
+
 	const btnLabels = {"submitLbl": "Submit", "deleteLbl": "Delete", "backLbl": "Back", "confirmationDelMsg": "Delete document "};
 	const file_category: string = "document_character";
-
 
 	const { form, errors, constraints, enhance, delayed, message } = superForm(
 		data.form, {
@@ -29,8 +27,11 @@
 		}
 	);
 
+	const searchPageUrl: string = `${InternalURLs.document}/${data.document_id}`
+	const characterUrl: string = `${InternalURLs.character}/${$form.charMain_key}`
+
 	function regenerateImage() {
-		alert("Not implemented... yet");
+		alert("Not implemented yet, but you can ask Clio to: generate an image for character [character name]");
 	}
 
 </script>
@@ -38,7 +39,7 @@
 <SubmitToast message={$message} page_status={$page.status}/>
 
 <div class="container mx-auto p-6">
-	<h1 class="text-2xl font-bold mb-4">{$form.name}</h1>
+	<h1 class="text-2xl font-bold mb-4">{$form.character_name}</h1>
 	<div class="flex">
 		<!-- Display the Image -->
 		<div class="w-1/3 pr-4 border-gray-200 border">
@@ -58,12 +59,15 @@
 				<input type="hidden" id="charMain_key" name="charMain_key" bind:value={$form.charMain_key} />
 				<input type="hidden" id="docMain_key" name="docMain_key" bind:value={$form.docMain_key} />
 				<input type="hidden" id="imageFilename" name="imageFilename" bind:value={$form.imageFilename} />
-				<TextField label="Name" id="name" bind:value={$form.name}
-									 errors={$errors.name} constraints={$constraints.name} />
+				<input type="hidden" id="character_name" name="character_name" bind:value={$form.character_name} />
+				<input type="hidden" id="document_title" name="document_title" bind:value={$form.document_title} />
+				<LinkField id="character_name" url={characterUrl} label="Character" value={$form.character_name} />
+				<LinkField id="document_title" url={searchPageUrl} label="Document" value={$form.document_title} />
+
 				<div class="mb-2">
 					<span class="inline-flex items-center py-0.5">
 							<FieldLabel id="prompt" label="Prompt"/>
-							<DispatchButton icon={ImagePlus} label="Regenerate image"  on:dispatchButtonClick={() => regenerateImage()}/>
+							<DispatchButton icon={ImagePlus} label="Generate image"  on:dispatchButtonClick={() => regenerateImage()}/>
 					</span>
 				</div>
 				<TextAreaField id="prompt" bind:value={$form.prompt}
@@ -71,7 +75,7 @@
 				<div>
 					<FormButtons submitLbl={btnLabels.submitLbl} deleteLbl={btnLabels.deleteLbl} backLbl={btnLabels.backLbl}
 											 delayed={$delayed} objectId={$form._key} confirmationDelMsg={''.concat(btnLabels.confirmationDelMsg,
-											 $form.name.toString(), "?")}
+											 $form.character_name.toString(), "?")}
 											 backUrl={searchPageUrl}/>
 				</div>
 			</form>
