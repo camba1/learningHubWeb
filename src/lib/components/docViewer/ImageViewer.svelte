f2d9c64c26bd0f69b7b08c3926720a5c5ce89398
jperedo <peredo01@gmail.com>
2024-10-30 17:42:07 -0500

    Reloads images when url changes

--- src/lib/components/docViewer/ImageViewer.svelte
+++ src/lib/components/docViewer/ImageViewer.svelte
@@ -1,18 +1,32 @@
 <script lang="ts">
-
 	import { InternalURLs } from '$lib/utils/urls';
 
-	export let encodedFilename: string ;
+	export let encodedFilename: string;
 	export let alt: string = '';
 	export let file_category: string = 'document';
-	export let img_class: string = "h-auto max-w-full"
+	export let img_class: string = "h-auto max-w-full";
 	export let encodedParentFilename: string = '';
 
-	let url = `${InternalURLs.fileProxy}?filename=${encodedFilename}&file_category=${file_category}`
-	if (encodedParentFilename) {
-		url =`${url}&parent_filename=${encodedParentFilename}`
+	$: url = `${InternalURLs.fileProxy}?filename=${encodedFilename}&file_category=${file_category}`;
+	$: if (encodedParentFilename) {
+		url = `${url}&parent_filename=${encodedParentFilename}`;
 	}
-	console.log(url);
+
+	// let objUrl: string | null | unknown = null;
+	let objUrl: string = "";
+	let error: Error | null = null;
+
+	$: preload(url).then(
+		(newObjUrl) => {
+			objUrl = newObjUrl;
+			error = null;
+		},
+		(err) => {
+			error = err;
+			objUrl = "";
+		}
+	);
+
 	const preload = async (url: string) => {
 		const resp = await fetch(url);
 
@@ -22,17 +36,17 @@
 
 		const blob = await resp.blob();
 
-		return new Promise(function (resolve) {
-			let objUrl = URL.createObjectURL(blob)
+		return new Promise<string>(function (resolve) {
+			let objUrl = URL.createObjectURL(blob);
 			resolve(objUrl);
 		});
 	};
 </script>
 
-{#await preload(url)}
-	Loading....
-{:then objUrl}
-	<img src="{String(objUrl)}" alt={alt} class={img_class}/>
-{:catch error}
+{#if error}
 	<span>No Image<br>available</span>
-{/await}
+{:else if objUrl}
+	<img src={objUrl} alt={alt} class={img_class} />
+{:else}
+	Loading....
+{/if}
