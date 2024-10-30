81305cee8f5bafa16c47d21770d33a51505602b1
jperedo <peredo01@gmail.com>
2024-10-30 17:43:00 -0500

    Added Book images section

--- src/routes/private/character/[[id]]/+page.svelte
+++ src/routes/private/character/[[id]]/+page.svelte
@@ -19,6 +19,7 @@
 	const searchPageUrl: string = InternalURLs.characters
 	const btnLabels = {"submitLbl": "Submit", "deleteLbl": "Delete", "backLbl": "Back", "confirmationDelMsg": "Delete document "};
 	const file_category: string = "character";
+	const doc_file_category: string = "document_character";
 
 
 	const { form, errors, constraints, enhance, delayed, message } = superForm(
@@ -31,6 +32,18 @@
 
 	function regenerateImage() {
 		alert("Not implemented... yet");
+	}
+
+	let bookTitles = [
+		{ id: 1, title: 'Book One', imageFilename: 'Masterji.png', parentFilename: 'Croak.pdf', document_key: '1734621', document_character_key: "1824878" },
+		{ id: 2, title: 'Book Two', imageFilename: 'Rani.png', parentFilename: 'Croak.pdf', document_key: '1734621', document_character_key: "1824878" },
+		// Add more book data as needed
+	];
+
+	let selectedBook = null;
+
+	function selectBook(book) {
+		selectedBook = book;
 	}
 
 </script>
@@ -49,27 +62,69 @@
 				{/if}
 			</div>
 
-		<div class="w-2/3 border-gray-200 px-4 ml-4 border">
+		<div class="w-2/3  px-4 ml-4">
 			<!-- Display the Prompt in a Textarea -->
-			<form method="POST" use:enhance>
-				<input type="hidden" id="_key" name="_key" bind:value={$form._key} />
-				<TextField label="Name" id="name" bind:value={$form.name}
-									 errors={$errors.name} constraints={$constraints.name} />
-				<div class="mb-2">
-					<span class="inline-flex items-center py-0.5">
-							<FieldLabel id="prompt" label="Prompt"/>
-							<DispatchButton icon={ImagePlus} label="Generate image"  on:dispatchButtonClick={() => regenerateImage()}/>
-					</span>
+			<div>
+				<div id="docTabs" role="tablist" class="tabs tabs-lifted">
+					<input type="radio" name="docDetailsTabsGroup" role="tab" class="tab" aria-label="Details" checked />
+					<div role="tabpanel" class="tab-content bg-base-100 border-base-300 rounded-box p-6">
+						<div>
+							<form method="POST" use:enhance>
+								<input type="hidden" id="_key" name="_key" bind:value={$form._key} />
+								<TextField label="Name" id="name" bind:value={$form.name}
+													 errors={$errors.name} constraints={$constraints.name} />
+								<div class="mb-2">
+									<span class="inline-flex items-center py-0.5">
+											<FieldLabel id="prompt" label="Prompt"/>
+											<DispatchButton icon={ImagePlus} label="Generate image"  on:dispatchButtonClick={() => regenerateImage()}/>
+									</span>
+								</div>
+								<TextAreaField id="prompt" bind:value={$form.prompt}
+															 errors={$errors.prompt} constraints={$constraints.prompt} />
+								<div>
+									<FormButtons submitLbl={btnLabels.submitLbl} deleteLbl={btnLabels.deleteLbl} backLbl={btnLabels.backLbl}
+															 delayed={$delayed} objectId={$form._key} confirmationDelMsg={''.concat(btnLabels.confirmationDelMsg,
+															 $form.name.toString(), "?")}
+															 backUrl={searchPageUrl}/>
+								</div>
+							</form>
+						</div>
+					</div>
+					<input type="radio" name="docDetailsTabsGroup" role="tab" class="tab" aria-label="Book Images"  />
+					<div role="tabpanel" class="tab-content bg-base-100 border-base-300 rounded-box p-6">
+						<!-- Books Display -->
+						<div class="flex">
+							<!-- Book Titles -->
+							<div class="w-1/3 space-y-2">
+								{#each bookTitles as book}
+									<div>
+										<button class="btn btn-ghost" on:click|preventDefault={() => selectBook(book)}>{book.title}</button>
+									</div>
+								{/each}
+							</div>
+							<div class="divider divider-horizontal"/>
+							<!-- Selected Book -->
+							<div class="w-2/3 pl-4">
+								{#if selectedBook}
+									<div class="card shadow-lg">
+										<figure>
+										<ImageViewer encodedFilename={selectedBook.imageFilename} file_category={doc_file_category}
+																 encodedParentFilename={selectedBook.parentFilename}
+																 alt="Image generated for book {selectedBook.title}" img_class="w-full h-auto object-cover"/>
+										</figure>
+										<div class="card-body items-center text-center">
+											<a href={InternalURLs.document +"/" + selectedBook.document_key +  "/document_character/" + selectedBook.parentFilename + "/" + selectedBook.document_character_key} class="link link-primary">{$form.name}</a> in
+											<a href={InternalURLs.document +"/" + selectedBook.document_key} class="link link-primary">{selectedBook.title}</a>
+										</div>
+									</div>
+								{:else}
+									<div>Please select a book</div>
+								{/if}
+							</div>
+						</div>
+					</div>
 				</div>
-				<TextAreaField id="prompt" bind:value={$form.prompt}
-											 errors={$errors.prompt} constraints={$constraints.prompt} />
-				<div>
-					<FormButtons submitLbl={btnLabels.submitLbl} deleteLbl={btnLabels.deleteLbl} backLbl={btnLabels.backLbl}
-											 delayed={$delayed} objectId={$form._key} confirmationDelMsg={''.concat(btnLabels.confirmationDelMsg,
-											 $form.name.toString(), "?")}
-											 backUrl={searchPageUrl}/>
-				</div>
-			</form>
+			</div>
 		</div>
 	</div>
 </div>
