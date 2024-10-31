7233f081a3165fb7ad910a46a5c7a8f22953048d
jperedo <peredo01@gmail.com>
2024-10-31 13:14:17 -0500

    initial commit

---
+++ src/lib/server/utils/fetchUtils.ts
@@ -0,0 +1,11 @@
+export function get_standard_options( sort_by: string = '', sort_order = 'asc',
+																			created_by: string = '', updated_by: string = '' ): { [key: string]: string }{
+
+	const options: { [key: string]: string } = {}
+	if (sort_by) options['sort_by']= sort_by;
+	if (sort_order) options['sort_order'] = sort_order;
+	if (created_by) options['created_by'] = created_by;
+	if (updated_by) options['updated_by'] = updated_by;
+	return options
+}
+
