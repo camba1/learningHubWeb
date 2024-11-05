c3d391d66dc3f84ca5a2ac04e84fdb05c824e2e0
jperedo <peredo01@gmail.com>
2024-11-05 17:03:11 -0600

    Moved _stringToEncoded to its own file for reusability

--- src/routes/private/fileProxy/+server.ts
+++ src/routes/private/fileProxy/+server.ts
@@ -2,6 +2,7 @@ import { error } from '@sveltejs/kit';
 import { ExternalURLs } from '$lib/server/utils/externalUrls';
 import { APIFileClient } from '$lib/api/apiFileClient';
 import { getAuthToken } from '$lib/server/utils/headers';
+import { stringToEncoded } from '$lib/server/utils/fetchUtils';
 
 export const GET = async ({ request,cookies }) => {
 	const url = new URL(request.url);
@@ -10,19 +11,9 @@ export const GET = async ({ request,cookies }) => {
 	const parent_filename = url.searchParams.get('parent_filename') || "";
 	if (filename == "") throw error(404, 'Filename not found.');
 
-	const encodedFilename = _stringToEncoded(filename)
-	const encodedFileCategory = _stringToEncoded(file_category)
+	const encodedFilename = stringToEncoded(filename)
+	const encodedFileCategory = stringToEncoded(file_category)
 	const api_client = new APIFileClient(ExternalURLs.download_file);
-	// console.log("parent_filename: ", parent_filename)
+
 	return api_client.download_file(encodedFilename, getAuthToken(cookies), encodedFileCategory, parent_filename);
 };
-
-function _stringToEncoded(str: string) {
-	const urlEncodedRegex = /%[0-9A-Fa-f]{2}/;
-
-	if(urlEncodedRegex.test(str)){
-		return str;
-	} else {
-		return encodeURIComponent(str);
-	}
-}
