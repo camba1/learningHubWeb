1a3349ef1baf46ade7117226f8a2e454f7d7c946
jperedo <peredo01@gmail.com>
2024-11-06 11:58:10 -0600

    Cleanup and add ability to save page contents via API

--- src/lib/components/docViewer/AudioFilesViewer.svelte
+++ src/lib/components/docViewer/AudioFilesViewer.svelte
@@ -3,6 +3,7 @@
 	import AudioPlayer from '$lib/components/audioPlayer/AudioPlayer.svelte';
 	import FileLabel from '$lib/components/form/FieldLabel.svelte';
 	import type { PageSchemaType } from '$lib/schemas/documentDetailPages';
+	import { InternalURLs } from '$lib/utils/urls';
 
 
 	let selectedFile: PageSchemaType;
@@ -10,23 +11,49 @@
 	export let availableFiles: PageSchemaType[];
 	export let label: string;
 
-</script>
-{#if label}
-<!--	<label for="file-select">Select a page:</label>-->
-	<FileLabel id="file-select" label={label} />
-{/if}
-<select id="file-select" bind:value={selectedFile} >
-	{#each availableFiles as file}
-		{#if file.audioFilename}
-			<option value={file}>{file.friendlyName}  &#9205;</option>
-		{:else}
-			<option value={file}>{file.friendlyName} </option>
-		{/if}
-	{/each}
-</select>
+	// Define the event detail type
+	interface SaveEventDetail {
+		filename: string;
+		content: string;
+	}
 
+	// Function to handle the event from TextViewer
+	async function updateContent(event: CustomEvent<SaveEventDetail>) {
+		const { filename, content } = event.detail;
+		const file_category = 'document';
+
+			await fetch(InternalURLs.updateFile, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({ filename, content, file_category })
+			});
+	}
+
+</script>
+<div class="mb-6">
+	{#if label}
+		<FileLabel id="file-select" label={label} />
+	{/if}
+	<select id="file-select" bind:value={selectedFile} >
+		{#each availableFiles as file}
+			{#if file.audioFilename}
+				<option value={file}>{file.friendlyName}  &#9205;</option>
+			{:else}
+				<option value={file}>{file.friendlyName} </option>
+			{/if}
+		{/each}
+	</select>
+</div>
 {#if selectedFile}
-	<TextViewer encodedFilename={selectedFile.filename} label="Page Text" id="pages" readOnly={false}/>
+	<div class="mb-4">
+		<TextViewer encodedFilename={selectedFile.filename}
+								label="Page Text" id="pages" readOnly={false}
+								btnLabel={"Save " + selectedFile.friendlyName.toLowerCase()}
+								on:txtViewerBtnClick={updateContent}
+		/>
+	</div>
 	{#if selectedFile.audioFilename}
 		<AudioPlayer src={selectedFile.audioFilename} title="Page Audio" artist="" />
 	{/if}
--- src/lib/components/docViewer/TextViewer.svelte
+++ src/lib/components/docViewer/TextViewer.svelte
@@ -1,13 +1,17 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
+	import { createEventDispatcher } from 'svelte';
 	import FieldLabel from '$lib/components/form/FieldLabel.svelte';
+	import DispatchButton from '$lib/components/genericControls/DispatchButton.svelte';
+	import { Save } from 'lucide-svelte';
 
 	export let label: string
 	export let id: string
 	export let readOnly: boolean = false;
-
-
 	export let encodedFilename: string;
+	export let btnLabel: string = "";
+
+	const dispatch = createEventDispatcher();
 	let textContent = '';
 	let fileUrl = '';
 
@@ -29,10 +33,25 @@
 		})()
 	}
 
+	// Dispatch an event with the necessary data
+	function btnClick() {
+		alert('btnClick')
+		dispatch('txtViewerBtnClick', { filename: encodedFilename, content: textContent });
+	}
+
 </script>
 
 {#if textContent}
-	<FieldLabel id={id} label={label}/>
+	<div>
+		<div class="inline-flex items-center py-0.5"><!-- Container for the label and button -->
+			<FieldLabel id={id} label={label} />
+			{#if btnLabel}
+				<span class="mr-1">
+					<DispatchButton icon={Save} label={btnLabel} on:dispatchButtonClick={btnClick} />
+				</span>
+			{/if}
+		</div>
+	</div>
 	<textarea
 		name={id}
 		id={id}
