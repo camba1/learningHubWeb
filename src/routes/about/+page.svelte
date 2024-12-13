1999a3dbb5f41db65962ccf545f24eae2c0a43cf
jperedo <peredo01@gmail.com>
2024-12-13 10:27:53 -0600

    Changed learning to imaging

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -6,10 +6,10 @@
 	<section id="about-us" class="container mx-auto pt-12 section-spacing">
 		<h2 class="text-3xl font-bold mb-8">About Us</h2>
 		<p>
-			At Bolbeck LLC, we believe that every child deserves a chance to explore the wonderful world of literature. That's why we created Bolbeck Learning, a platform powered by the latest AI technology that makes it easy for publishers and authors to distribute their books to kids around the world.
+			At Bolbeck LLC, we believe that every child deserves a chance to explore the wonderful world of literature. That's why we created Bolbeck Imagine, a platform powered by the latest AI technology that makes it easy for publishers and authors to distribute their books to kids around the world.
 		</p>
 		<p>
-			Our mission is to provide equal access to literature, no matter the language or location. With Bolbeck Learning, both parents and educators can introduce children to a diverse range of stories and characters, helping them develop a love for reading and learning.
+			Our mission is to provide equal access to literature, no matter the language or location. With Bolbeck Imagine, both parents and educators can introduce children to a diverse range of stories and characters, helping them develop a love for reading and learning.
 		</p>
 	</section>
 
