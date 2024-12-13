38ad109d739fada22109afd008a7df25b0db602c
jperedo <peredo01@gmail.com>
2024-12-13 11:28:54 -0600

    Changed learning for imagine

--- src/lib/components/genericControls/MainFooter.svelte
+++ src/lib/components/genericControls/MainFooter.svelte
@@ -1,7 +1,7 @@
 <footer class="container mx-auto pt-24 py-4">
 	<div class="flex flex-col sm:flex-row items-center sm:items-stretch justify-center sm:justify-between">
 		<div class="pr-8 mb-4 sm:mb-0">
-			<h2 class="text-xl font-bold">BolbeckLearning</h2>
+			<h2 class="text-xl font-bold">Bolbeck Imagine</h2>
 			<p class="text-sm text-gray-600">Learning for kids of all ages</p>
 		</div>
 		<div class="sm:flex sm:flex-row sm:items-stretch justify-center sm:justify-end">
