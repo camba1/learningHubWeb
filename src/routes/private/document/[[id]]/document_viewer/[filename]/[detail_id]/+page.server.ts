a92e55ead6f57aff2f26b4a61dbca7dc3cdd623c
jperedo <peredo01@gmail.com>
2024-12-16 14:49:55 -0600

    Moved bulk of the code to components and helper functions so that we can reuse it elsewhere

--- src/routes/private/document/[[id]]/document_viewer/[filename]/[detail_id]/+page.server.ts
+++ src/routes/private/document/[[id]]/document_viewer/[filename]/[detail_id]/+page.server.ts
@@ -1,52 +1,6 @@
-import { error } from '@sveltejs/kit';
-import { fetchDocumentDetailPages } from '$lib/api/documentDetailPages';
-import type { PageSchemaType } from '$lib/schemas/documentDetailPages';
-import { type DocumentImageSchemaType } from '$lib/schemas/documentImages';
-import { getAuthToken } from "$lib/server/utils/headers";
-import { fetchDocumentImageLookup } from '$lib/api/documents';
+import { getViewerData } from '$lib/utils/viewerData';
 
 
-export async function load({ params, cookies, url }) {
-
-	let docDetailPages = null;
-	let docImages = null;
-
-	let editable: boolean = false;
-	if ((url.searchParams.has("edit")) && url.searchParams.get("edit")) {
-		const edit_param = url.searchParams.get("edit") ?? "false";
-		if (edit_param === "true") {
-			editable = true;
-		}
-	}
-
-	if (params.id) {
-		[docDetailPages, docImages] = await Promise.all([
-			fetchDocumentDetailPages(getAuthToken(cookies),0, 10, "created_at", "desc", params.detail_id),
-			fetchDocumentImageLookup(params.id, getAuthToken(cookies),0,0, 1000, "pageNumber")
-		]);
-	}
-	if (docDetailPages && docDetailPages.length > 1) throw error(404, 'Found more than one pages detail record');
-
-	let availableFiles: PageSchemaType[] = [];
-	let number_of_pages = 0;
-	if (docDetailPages && docDetailPages.length == 1) {
-		availableFiles = docDetailPages[0].pages
-		availableFiles.sort((a, b) => a.number - b.number);
-		number_of_pages = availableFiles.length
-	}
-
-	let availableImages: DocumentImageSchemaType[] = [];
-	if (docImages) {
-		availableImages = docImages;
-		// availableImages.sort((a, b) => a.pageNumber - b.pageNumber);
-	}
-
-	return {
-		editable: editable,
-		parentFilename: params.filename,
-		number_of_pages: number_of_pages,
-		availableFiles: availableFiles,
-		availableImages: availableImages
-	};
-
+export async function load({ params, cookies }) {
+	return getViewerData(true, params.id ?? '', params.detail_id, params.filename, cookies)
 }
--- src/routes/private/document/[[id]]/document_viewer/[filename]/[detail_id]/+page.svelte
+++ src/routes/private/document/[[id]]/document_viewer/[filename]/[detail_id]/+page.svelte
@@ -1,101 +1,13 @@
 <script lang="ts">
 	import type { PageData } from './$types.js';
-	import { page } from '$app/stores';
-	import { fade } from 'svelte/transition';
-	import { Image } from 'lucide-svelte';
-	import { Headphones, CircleArrowLeft } from 'lucide-svelte';
-	import type { PageSchemaType } from '$lib/schemas/documentDetailPages';
-	import type { DocumentImageSchemaType } from '$lib/schemas/documentImages';
-	import ImageViewer from '$lib/components/docViewer/ImageViewer.svelte';
-	import AudioFilesROViewer from '$lib/components/pageViewer/AudioFilesROViewer.svelte';
-	import LinkButton from '$lib/components/genericControls/LinkButton.svelte';
-	import { InternalURLs } from '$lib/utils/urls.js';
+	import ViewerUI from '$lib/components/pageViewer/ViewerUI.svelte';
 
 	export let data: PageData;
 
-	let currentPageNumber = 0;
-	let currentPage: PageSchemaType = data.availableFiles[currentPageNumber];
-	let currentImage: DocumentImageSchemaType | undefined = data.availableImages[currentPageNumber];
-	let image_file_category: string = "document";
-
-	// Key for forcing transition
-	let imageKey = currentImage?.imageFilename || 'initial';
-
-	function set_image_file_type(){
-		image_file_category = "document_image";
-		if (currentPageNumber === 0) {
-			image_file_category = "document";
-		}
-	}
-
-	function changeCurrentFileIndex(event: CustomEvent<{ index: number }>) {
-		currentPageNumber = event.detail.index;
-		currentPage = data.availableFiles[currentPageNumber];
-		currentImage = data.availableImages.find(image => image.pageNumber === currentPage.number);
-		set_image_file_type();
-		// Update keys to trigger transitions
-		imageKey = currentImage?.imageFilename || 'changed';
-	}
-
 </script>
-
-<div class="w-full h-full flex flex-col p-10 relative">
-	{#if !data.availableFiles}
-		<p class="text-center text-error p-4">No data found...</p>
-	{:else}
-		<!-- LinkButton at the top right corner -->
-		{#if !data.editable}
-			<div class="absolute top-0 right-0 mt-2 mr-2 z-10">
-				<LinkButton label="X" href="{InternalURLs.documents}" btn_additional_class="btn-outline btn-success"/>
-			</div>
-		{/if}
-
-		<div class="flex-1 flex flex-col overflow-hidden">
-			<div class="card w-full h-full flex flex-col shadow-xl overflow-hidden card-bordered border-gray-200">
-				<!-- Image at the top of the card (fixed height) -->
-				<figure class="px-2 pt-4 pb-1 h-[63%] min-h-0">
-					{#if currentImage && currentImage.imageFilename}
-						{#key imageKey}
-							<div in:fade="{{ duration: 300}}" class="w-full h-full">
-								<ImageViewer
-									encodedFilename={currentImage.imageFilename}
-									file_category={image_file_category}
-									encodedParentFilename={data.parentFilename}
-									alt="Image generated for page {currentPage.number}"
-									img_class="rounded-xl w-full h-full object-contain"
-								/>
-							</div>
-						{/key}
-					{:else}
-						<div class="text-center text-neutral-content flex items-center justify-center w-full h-full">
-							No Image available
-						</div>
-					{/if}
-				</figure>
-
-				<!-- Content at the bottom of the card (flexible height) -->
-				<div class="card-body flex-1 overflow-auto p-2 pt-2">
-					<div class="flex flex-col items-center">
-						<!-- Conditional Edit Button -->
-						{#if data.editable}
-							<div class="flex flex-row items-center justify-center space-x-4">
-								<LinkButton label="Edit Audio" icon={Headphones} href={encodeURI(`${InternalURLs.document}/${$page.params.id}/document_audio/${data.parentFilename}/${$page.params.detail_id}?page=${currentPage.number}`)}  />
-								<LinkButton label="Edit Image" icon={Image} href={encodeURI(`${InternalURLs.document}/${$page.params.id}/document_image/${data.parentFilename}/${currentImage?._key ?? ''}`)} />
-								<LinkButton label="Back" icon={CircleArrowLeft} href={encodeURI(`${InternalURLs.document}/${$page.params.id}/document_detail/${data.parentFilename}/${$page.params.detail_id}`)} />
-							</div>
-						{/if}
-						<div class="w-full flex-1">
-							{#if data.availableFiles}
-								<AudioFilesROViewer
-									availableFiles={data.availableFiles}
-									label="Jump to Page:"
-									on:fileIndexChange={changeCurrentFileIndex}
-								/>
-							{/if}
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	{/if}
-</div>
+<ViewerUI
+	availableFiles={data.availableFiles}
+	availableImages={data.availableImages}
+	editable={data.editable}
+	parentFilename={data.parentFilename}
+/>
