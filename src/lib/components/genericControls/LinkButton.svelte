f1c4e7e1d5a990ef2b4a3f806d1e803b4af29b67
jperedo <peredo01@gmail.com>
2024-12-16 14:48:48 -0600

    Cleanup and simplification

--- src/lib/components/genericControls/LinkButton.svelte
+++ src/lib/components/genericControls/LinkButton.svelte
@@ -1,3 +1,35 @@
+<!--<script lang="ts">-->
+<!--	/**-->
+<!--	 * @param icon The Lucide icon component-->
+<!--	 * @param label The button label-->
+<!--	 * @param href The link to navigate to when the button is clicked-->
+<!--	 * @param btn_class The button class. Default: "btn btn-xs mx-0.5"-->
+<!--	 */-->
+
+<!--	import type { ComponentType } from 'svelte';-->
+<!--	export let icon: ComponentType | null = null;-->
+<!--	export let label: string = "";-->
+<!--	export let href: string;-->
+<!--	export let btn_additional_class: string = "";-->
+
+<!--</script>-->
+
+<!--	<button class="btn btn-xs mx-0.5 {btn_additional_class}">-->
+<!--		{#if label}-->
+<!--			<a href={href}>-->
+<!--				{#if icon}-->
+<!--					<svelte:component this={icon} class="w-4 h-4" />-->
+<!--				{/if}-->
+<!--				{label}-->
+<!--			</a>-->
+<!--		{:else if icon}-->
+<!--			<a href={href}><svelte:component this={icon} class="w-4 h-4" /></a>-->
+<!--		{:else}-->
+<!--			<a href={href}>I need a label</a>-->
+<!--		{/if}-->
+<!--	</button>-->
+
+
 <script lang="ts">
 	/**
 	 * @param icon The Lucide icon component
@@ -11,18 +43,17 @@
 	export let label: string = "";
 	export let href: string;
 	export let btn_additional_class: string = "";
-
 </script>
 
-	<button class="btn btn-xs mx-0.5 {btn_additional_class}">
-		{#if label}
-			{#if icon}
-				<svelte:component this={icon} class="w-4 h-4" />
-			{/if}
-			<a href={href}>{label}</a>
-		{:else if icon}
-			<a href={href}><svelte:component this={icon} class="w-4 h-4" /></a>
-		{:else}
-			<a href={href}>I need a label</a>
+<button class="btn btn-xs mx-0.5 {btn_additional_class}">
+	<a href={href} class="flex items-center space-x-1">
+		{#if icon}
+			<svelte:component this={icon} class="w-4 h-4" />
 		{/if}
-	</button>
+		{#if label}
+			<span>{label}</span>
+		{:else if icon}
+			<span>I need a label</span>
+		{/if}
+	</a>
+</button>
