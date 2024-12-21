6a2e8ce3deac74f87a7fb89f9b9a637d0cf72600
jperedo <peredo01@gmail.com>
2024-12-20 22:04:01 -0600

    Text cleanup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,11 +8,12 @@
 		<div class="flex flex-wrap items-center">
 			<div class="w-full md:w-1/2">
 				<h1 class="text-5xl font-bold mb-4">
-					Enhancing Children's Literacy Worldwide
+					Bring your children's books to life
 				</h1>
-				<p class="text-xl mb-8 mr-3">
-					Discover the magic of books with Bolbeck Imagine, the cutting-edge <b>AI</b> platform
-					from Bolbeck LLC that simplifies the distribution of children's books.
+				<p class="text-xl mb-8 mt-3 mr-3">
+					Uncover the magic of books with Bolbeck Imagine, the cutting-edge <b>AI</b> platform
+					that enhances and simplifies the creation and distribution of children's books. <br> <br>
+					Make children worldwide fall in love with reading all over again!
 				</p>
 				<a href="/sign-up" class="btn btn-primary">Get Started</a>
 			</div>
@@ -30,20 +31,23 @@
 		<h2 class="text-3xl font-bold mb-8">Features</h2>
 
 		<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-			<BasicCard title="Book upload" description="Upload PDF and MS Word versions of your books easily to our platform."/>
-			<BasicCard title="Track book information with ease" description="Build a library of your books. Automatically create book summaries and main character lists"/>
-			<BasicCard title="Translation" description="Translate books to multiple languages, making them accessible to readers around the world."/>
-			<BasicCard title="Audio" description="Create audio versions of the books for listening on the go or in the car."/>
-			<BasicCard title="Chat with your books" description="Easily search your books for important bits of information and ask questions."/>
-			<BasicCard title="Bring characters to life" description="Generate images for your books and your protagonists"/>
+			<BasicCard title="Track information with ease" description="Upload your books to automatically extract relevant information about its plot and protagonists"/>
+			<BasicCard title="Translate" description="Experience your books in multiple languages & make them accessible to readers around the world."/>
+			<BasicCard title="Listen" description="Create audio versions of the books for listening on the go or in the car."/>
+			<BasicCard title="Chat with your books" description="Find what you need in a flash by chatting with your book questions with ease"/>
+			<BasicCard title="Bring characters to life" description="Generate images to make your books and protagonists come alive"/>
+			<BasicCard title="Enjoy your books" description="Create an experience that allows children to have fun reading again"/>
 		</div>
 
 		<div class="flex flex-wrap items-center mt-12">
 			<div class="w-full md:w-1/2">
-				<h3 class="text-2xl font-semibold mb-4">Let's Make Learning Exciting and Accessible</h3>
+				<h3 class="text-2xl font-semibold mb-4">Let's make reading exciting and accessible</h3>
 				<p class="text-lg mb-8 mr-3">
 					Bolbeck Imagine is a comprehensive platform that not only makes books accessible
 					but also brings them to life through audio, translation, and interactive chat.
+				</p>
+				<p class="text-lg mb-8 mr-3">
+					Not a professional writer? No problem! Enjoy time with your kids creating your shared stories. Just upload a document and let Bolbeck Imagine take care of the rest.
 				</p>
 			</div>
 			<div class="w-full md:w-1/2">
