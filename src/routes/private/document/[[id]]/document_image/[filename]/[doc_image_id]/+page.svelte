bb0c943b8db3121f68c0414e7af909cbc4c2c4ca
jperedo <peredo01@gmail.com>
2025-01-09 14:35:04 -0600

    Add animation related fields

--- src/routes/private/document/[[id]]/document_image/[filename]/[doc_image_id]/+page.svelte
+++ src/routes/private/document/[[id]]/document_image/[filename]/[doc_image_id]/+page.svelte
@@ -6,9 +6,10 @@
 	import { InternalURLs } from '$lib/utils/urls.js';
 	import FormButtons from '$lib/components/form/FormButtons.svelte';
 	import ImageViewer from '$lib/components/docViewer/ImageViewer.svelte';
+	import VideoPlayer from '$lib/components/docViewer/VideoPlayer.svelte';
 	import TextAreaField from '$lib/components/form/TextAreaField.svelte';
 	import SubmitToast from '$lib/components/form/SubmitToast.svelte';
-	import { ImagePlus } from 'lucide-svelte';
+	import { ImagePlus, Video } from 'lucide-svelte';
 	import DispatchButton from '$lib/components/genericControls/DispatchButton.svelte';
 	import FieldLabel from '$lib/components/form/FieldLabel.svelte';
 	import TextField from '$lib/components/form/TextField.svelte';
@@ -34,6 +35,10 @@
 		alert("Not implemented yet, but you can ask Clio to: generate image");
 	}
 
+	function animateImage() {
+		alert("Not implemented yet, but you can ask Clio to: animate image");
+	}
+
 </script>
 
 <SubmitToast message={$message} page_status={$page.status}/>
@@ -43,13 +48,21 @@
 	<div class="flex">
 		<!-- Display the Image -->
 		<div class="w-1/3 p-1.5 border-gray-200 border">
-			{#if $form.imageFilename}
-				<ImageViewer encodedFilename={$form.imageFilename} file_category={file_category}
-										 encodedParentFilename={data.parentFilename}
-										 alt="Image generated for this {file_category}" img_class="w-full h-auto object-contain"/>
-			{:else}
-				<span>No Image<br>available</span>
-			{/if}
+			<div class="pt-3">
+				{#if $form.imageFilename}
+					<ImageViewer encodedFilename={$form.imageFilename} file_category={file_category}
+											 encodedParentFilename={data.parentFilename}
+											 alt="Image generated for this {file_category}" img_class="w-full h-auto object-contain"/>
+				{:else}
+					<span>No Image<br>available</span>
+				{/if}
+			</div>
+			<div class="pt-10">
+				{#if $form.animationFilename}
+					<VideoPlayer encodedFilename={$form.animationFilename}
+											 encodedParentFilename={data.parentFilename} />
+				{/if}
+			</div>
 		</div>
 
 		<div class="w-2/3 border-gray-200 px-4 ml-4 border">
@@ -76,7 +89,18 @@
 				</div>
 				<TextAreaField id="prompt" bind:value={$form.prompt}
 											 errors={$errors.prompt} constraints={$constraints.prompt} />
+<!--				<div>-->
+
+				<div class="mb-3">
+				<span class="inline-flex items-center py-0.5">
+						<FieldLabel id="animationPrompt" label="Image animation prompt"/>
+						<DispatchButton icon={Video} label="Animate image"  on:dispatchButtonClick={() => animateImage()}/>
+				</span>
+				</div>
+				<TextAreaField id="animationPrompt" bind:value={$form.animationPrompt}
+											 errors={$errors.animationPrompt} constraints={$constraints.animationPrompt} />
 				<div>
+
 					<FormButtons submitLbl={btnLabels.submitLbl} deleteLbl={btnLabels.deleteLbl} backLbl={btnLabels.backLbl}
 											 delayed={$delayed} objectId={$form._key} confirmationDelMsg={''.concat(btnLabels.confirmationDelMsg,
 											  "?")}
